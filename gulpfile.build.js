2ea7ede83206b5e785c6eb6e9c2c3b66409825c6
eankeen <24364012+eankeen@users.noreply.github.com>
2020-01-24 20:13:39 -0800

    workflow: rework build step now script tags are automatically injected in proper html files. this tries to fix issue where css files in 'dist' directory are somehow different than the ones used in development

--- gulpfile.build.js
+++ gulpfile.build.js
@@ -1,30 +1,28 @@
 import { dest, src } from "gulp";
 import concat from "gulp-concat";
+import rename from "gulp-rename";
 import postcss from "gulp-postcss";
 import scss from "postcss-scss";
 import del from "del";
 
 let postCssPlugins = [
-  require("postcss-import")(),
-  require("postcss-strip-inline-comments"),
-  require("postcss-preset-env")(),
-  require("postcss-simple-vars")(),
-  require("autoprefixer")(),
   require("cssnano")()
 ];
 
 async function build() {
   del.sync(["dist/*", "!dist"]);
-  src("src/theme.light.css")
-    .pipe(concat("fox.light.min.css"))
+  src("site/css/fox.dark.css")
+    // .pipe(concat("fox.light.min.css"))
     .pipe(postcss(postCssPlugins, { syntax: scss }))
-    .pipe(dest("dist"));
-
-  src("src/theme.dark.css")
-    .pipe(concat("fox.dark.min.css"))
-    .pipe(postcss(postCssPlugins, { syntax: scss }))
+    .pipe(rename('fox.dark.min.css'))
     .pipe(dest("dist"))
-    .pipe(concat("fox.min.css"))
+    .pipe(rename('fox.min.css'))
+    .pipe(dest('dist'))
+
+  src("site/css/fox.light.css")
+    // .pipe(concat("fox.dark.min.css"))
+    .pipe(postcss(postCssPlugins, { syntax: scss }))
+    .pipe(rename("fox.light.min.css"))
     .pipe(dest("dist"))
 }
 
--- gulpfile.serve.js
+++ gulpfile.serve.js
@@ -1,7 +1,12 @@
+import path from "path";
+import fs from "fs";
+
 import { dest, parallel, series, src, watch } from "gulp";
 import concat from "gulp-concat";
-import postcss from "gulp-postcss";
 import plumber from "gulp-plumber";
+import inject from "gulp-inject";
+import rename from "gulp-rename";
+import postcss from "gulp-postcss";
 import scss from "postcss-scss";
 import bs from "browser-sync"
 
@@ -19,37 +24,107 @@ async function init() {
   browserSync.init({ server: "site" });
 }
 
+async function copyHtml() {
+//   watch(["samples/*.html"], { ignoreInitial: false }, async function copyHtmlWatchArg() {
+//     src(["samples/*.html", "!samples/index.html"])
+//       .pipe(plumber())
+//       .pipe(rename(path => {
+//         return {
+//           dirname: path.dirname,
+//           basename: path.basename,
+//           extname: ".pre" + path.extname
+//         }
+//       }))
+//       .pipe(dest("site/dark"))
+//       .pipe(dest("site/light"));
+
+//     src(["samples/index.html"])
+//       .pipe(plumber())
+//       .pipe(rename('index.pre.html'))
+//       .pipe(dest("site"))
+//   })
+}
+
+async function copyCss() {
+  // watch(["src/*.css"], { ignoreInitial: false }, async function copyCssWatchArgs() {
+  //   src([""])
+  // })
+}
+
 async function htmlReload() {
   watch(["samples/*.html"], { ignoreInitial: false }, async function htmlReloadWatchArg() {
     src(["samples/*.html", "!samples/index.html"])
-      .pipe(dest("site/dark"))
+      .pipe(inject(src('css/fox.dark.css', {
+        cwd: path.join(__dirname, 'site')
+      })))
+      .pipe(dest("site/dark"));
+
+    src(["samples/*.html", "!samples/index.html"])
+      .pipe(inject(src('css/fox.light.css', {
+        cwd: path.join(__dirname, 'site')
+      })))
       .pipe(dest("site/light"));
     
     src("samples/index.html")
+      .pipe(inject(src('css/fox.dark.css', {
+        cwd: path.join(__dirname, 'site')
+      })))
       .pipe(dest("site"));
     
     browserSync.reload();
   });
 }
+// async function htmlReload() {
+//   watch(["site/**/*.pre.html"], { ignoreInitial: false }, async function htmlReloadWatchArg() {
+//     src(["site/**/*.pre.html"])
+//       .pipe(plumber())
+//       .pipe(inject(src('css/fox.dark.css'), {
+//         relative: true
+//       }))
+//       .pipe(rename(path => ({
+//         dirname: path.dirname,
+//         basename: path.basename.slice(0, -4),
+//         extname: path.extname
+//       })))
+//       .pipe(dest("site"))
+//     //   .pipe(dest("site/dark"))
+//     //   .pipe(inject(src('site/css/fox.light.css'), {
+//     //     relative: true
+//     //   }))
+//     //   .pipe(dest("site/light"));
+    
+//     src("../samples/index.html", {
+//       cwd: path.join(__dirname, 'site')
+//     })
+//       .pipe(plumber())
+//       .pipe(inject(src('site/css/fox.dark.css'), {
+//         relative: false,
+//         addRootSlash: false
+//       }))
+//       .pipe(dest("site"));
+    
+//     browserSync.reload();
+//   });
+// }
 
 async function cssInject() {
   watch("src/*.css", { ignoreInitial: false }, async function cssInjectWatchArg() {
     src("src/theme.light.css")
       .pipe(plumber())
-      .pipe(concat("fox.css"))
+      .pipe(concat("fox.light.css"))
       .pipe(postcss(postCssPlugins, { syntax: scss }))
-      .pipe(dest("site/light/styles"))
+      .pipe(dest("site/css"))
       .pipe(browserSync.stream());
 
     src("src/theme.dark.css")
       .pipe(plumber())
-      .pipe(concat("fox.css"))
+      .pipe(concat("fox.dark.css"))
       .pipe(postcss(postCssPlugins, { syntax: scss }))
-      .pipe(dest("site/dark/styles"))
+      .pipe(dest("site/css"))
       .pipe(browserSync.stream());
   });
 }
 
-const serve = series(init, parallel(htmlReload, cssInject));
+const serve = series(parallel(copyHtml, copyCss), cssInject, htmlReload, init);
 
 export default serve;
